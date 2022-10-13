d9419ecec143b9f0af819c676e9384ed6ad3fbff
mustafaS0 <115041080+mustafaS0@users.noreply.github.com>
2022-10-13 22:24:26 +0300

    work1.java

---
+++ work1.java
@@ -0,0 +1,189 @@
+import java.io.CharArrayReader;
+import java.util.Scanner;
+
+import javax.swing.DefaultBoundedRangeModel;
+public class work1 {
+
+   
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+
+       /* 
+        int number = 1;
+        int remainder = number %2;
+        
+        boolean isPrime = true;
+
+        if(number == 1 ){
+            System.out.println("The number isn't prime");
+            return;
+        }
+        if(number < 1 ) {
+            System.out.println("The number is wrong");
+            return;
+        }
+
+        for (int i = 2; i<number ; i++) {
+            if(number % i ==0){
+                isPrime=false ;
+                
+            }
+            
+        }
+        if(isPrime){
+            System.out.println("The number is prime");
+        }else  {
+            System.out.println("The number isn't prime");
+        }*/
+        /*Scanner input = new Scanner(System.in);
+        System.out.println("Bir sesli harf giriniz.");
+        String m = input.nextLine();
+        
+        
+        
+        String [] character = new String[8];
+        character[0]= "A";
+        character[1]= "I";
+        character[2]= "O";
+        character[3]= "U";
+        character[4]= "a";
+        character[5]= "ı";
+        character[6]= "o";
+        character[7]= "u";
+        for (String harf1 : character) {
+            if(m == harf1){
+                System.out.println("Kalın Sesli HARF.");
+                return ;
+            }
+            
+        }
+        String[] characterOther = new String[8];
+        characterOther[0]="E";
+        characterOther[1]="İ";
+        characterOther[2]="Ö";
+        characterOther[3]="Ü";
+        characterOther[4]="e";
+        characterOther[5]="i";
+        characterOther[6]="ö";
+        characterOther[7]="ü";
+
+        for (String harf2 : characterOther) {
+            if (m==harf2){
+                System.out.println("İnce Sesli HARF.");
+                return;
+            }
+            
+        }*/
+
+
+        /*char character = 'k';
+
+        switch(character){
+            case 'A':
+            case 'O':
+            case 'I':
+            case 'U':
+            case 'a':
+            case 'o':
+            case 'ı':
+            case 'u':
+            System.out.println("Kalın sesli harf.");
+            break;
+            case 'E':
+            case 'İ':
+            case 'Ö':
+            case 'Ü':
+            case 'e':
+            case 'i':
+            case 'ö':
+            case 'ü':
+            System.out.println("İnce sesli harf.");
+            break;
+            default:
+            System.out.println("Sesli harf girmediniz.");
+            
+        }*/
+        
+       /*int number =9;
+       int total= 0;
+       boolean totalEsitMiNumber =true;
+        for (int i = 1; i < number; i++) {
+            if(number % i ==0){
+                total = total +i;
+            }
+        }
+        if(total == number ){
+            System.out.println("Mükemmel sayı.");
+        } else{
+            System.out.println("Mükemmel sayı değil.");
+        }*/
+            
+
+        /*int number1 =220;
+        int number2 = 284;
+        int total1 =0;
+        int total2 =0;
+
+        for (int i = 1; i < number1; i++) {
+            if(number1 % i ==0){
+                total1 = total1 + i;
+
+            }
+        }
+            
+        for (int a = 1; a < number2; a++) {
+            if(number2  % a ==0 ){
+                total2 = total2 + a;
+            }
+            
+        }if(total1==number2 && total2==number1){
+            System.out.println("Arkdaş sayılar.");
+        }else {
+            System.out.println("Arkadaş sayı değiller.");
+        }*/
+        
+       /* int [] sayilar = new int[]{1,2,3,4,5,6};
+        int find = 5;
+        boolean isThere = true;
+        
+        for (int number : sayilar) {
+            if(find==number){
+                System.out.println("I found already");
+                return;
+            }
+              }
+       
+              System.out.println("Unfortunately ı haven't.");*/
+        
+
+
+
+
+
+              /* int [] sayilar = new int[]{1,2,3,4,5,6};
+        int find = 5;
+        boolean isThere = true;
+        
+        for (int number : sayilar) {
+            if(find==number){
+                isThere=true;
+                return;
+            }
+              }
+       
+              if(isThere){
+                Sytem.out.println("Sayı mevcuttur.")
+              }else{
+
+                Sytem.out.println( "Sayı yok.")
+              }*/
+        
+
+
+
+              
+    }
+}
